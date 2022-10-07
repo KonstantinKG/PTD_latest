402768c65b28553aaa3a41cb54a741df9b7662fb
Kostya Goryachkovskiy <muhabrot@mail.ru>
2022-10-07 17:56:16 +0600

    Asgi testing

--- PTD/asgi.py
+++ PTD/asgi.py
@@ -1,15 +1,16 @@
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PTD.settings')
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PTD.settings')
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-django.setup()
 
 
 application = get_asgi_application()
