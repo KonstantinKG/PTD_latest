a39e5dd043bbfbd8a1d6c5989a45cd71d7228b8f
Kostya Goryachkovskiy <muhabrot@mail.ru>
2022-10-07 17:47:04 +0600

    Asgi testing

--- PTD/asgi.py
+++ PTD/asgi.py
@@ -3,19 +3,20 @@ import django
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PTD.settings')
 django.setup()
+application = get_asgi_application()
 
-application = ProtocolTypeRouter({
-   "http": get_asgi_application(),
-   "websocket": AuthMiddlewareStack(
-      URLRouter(
-         websocket_urlpatterns
-      )
-   )
-   ## IMPORTANT::Just HTTP for now. (We can add other protocols later.)
-})
+# application = ProtocolTypeRouter({
+#    "http": get_asgi_application(),
+#    "websocket": AuthMiddlewareStack(
+#       URLRouter(
+#          websocket_urlpatterns
+#       )
+#    )
+#    ## IMPORTANT::Just HTTP for now. (We can add other protocols later.)
+# })
