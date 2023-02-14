1832c402cf62c195f07d19ded19a847bb0a5047e
Kostya <muhabrot@mail.ru>
2023-02-14 20:06:55 +0600

    Db new settings v2 localhost

--- PTD/settings.py
+++ PTD/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
-        'HOST': '45.8.99.141',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
