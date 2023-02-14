6b96602c9663deea38aac014cc8cafbe3f3f9262
Kostya <muhabrot@mail.ru>
2023-02-14 20:12:10 +0600

    Db new settings v4 no port ho host

--- PTD/settings.py
+++ PTD/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
-        'HOST': '127.0.0.1',
+        'HOST': '',
         'PORT': '',
     }
 }
