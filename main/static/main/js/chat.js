b7f665de230c5379a8561996d06796351fcfb31b
Kostya <muhabrot@mail.ru>
2023-02-15 15:41:09 +0600

    JS wss connection instead ws

--- main/static/main/js/chat.js
+++ main/static/main/js/chat.js
@@ -7,7 +7,7 @@ const chat_aside_body = document.querySelector('.chat-aside__content')
 const userNickname = JSON.parse(document.getElementById('json-username').textContent);
 let is_scroll = true;
 
-let link = 'ws://' + window.location.host + '/ws/' + 'chat/',
+let link = 'wss://' + window.location.host + '/ws/' + 'chat/',
    timezoneOffset = new Date().getTimezoneOffset();
 
 if (window.location.protocol == 'https:')
